c0d750297627e1a6960a04f730bd9dcd5fc69616
javii46 <javi_14_dh@hotmail.com>
2014-02-27 17:25:07 +0100

    plantilla para jinja 2 y programa con salida html

--- html-weather.py
+++ html-weather.py
@@ -1,38 +1,42 @@
-#encoding=utf-8
+# coding: utf-8
 import json
 import requests
+from jinja2 import template
+import webbrowser
+
+
+
 
 def funcion(orientacion):
-	if orientacion == 90:
+	if orientacion > "337,5" and orientacion <= "22,5":
+		return 'N'
+	if orientacion > "22,5" and orientacion <= "67,5":
+		return 'NE'
+	if orientacion > "67,5" and orientacion <= "112,5":
 		return 'E'
-	if orientacion > 90 and orientacion < 180:
+	if orientacion > "112,5" and orientacion <= "157,5":
 		return 'SE'
-	if orientacion == 180:
+	if orientacion > "157,5" and orientacion <= "202,5":
 		return 'S'
-	if orientacion > 180 and orientacion < 270:
+	if orientacion > "202,5" and orientacion <= "247,5":
 		return 'SO'
-	if orientacion == 270:
+	if orientacion > "247,5" and orientacion <= "292,5":
 		return 'O'
-	if orientacion > 270:
-		return 'NO'
+	if orientacion > "292,5" and orientacion <= "337,5":
 
-print 'Lista de las ciudades.' """
+variable = open('plantilla','w')
 
-1. Almeria
-2. Cadiz
-3. Cordoba
-4. Granada
-5. Huelva
-6. Jaen
-7. Malaga
-8. Sevilla
+html = ''
 
-"""
+for linea in variable
+	html += linea
 
-diccionario = {"1":"Almeria","2":"Cadiz","3":"Cordoba","4":"Granada","5":"Huelva","6":"Jaen","7":"Malaga","8":"Sevilla"}
+plantilla = Template(html)
 
-consulta = raw_input("Introduce el número de la ciudad para saber sus condiciones meteorologicas: ")
-prov=diccionario[consulta]
+prov = ['Almería', 'Cádiz', 'Córdoba', 'Huelva', 'Jaén', 'Málaga', 'Sevilla']
+
+consulta1 = raw_input("Introduce el número de la ciudad para saber sus condiciones meteorologicas: ")
+prov=diccionario[consulta1]
 respuesta=requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,%s' % (prov, "spain")})
 dicc=json.loads(respuesta.text)
 temperaturamaxima=dicc["main"]["temp_max"] - 273
@@ -41,9 +45,14 @@ viento=dicc["wind"]["speed"]
 orientacion=dicc["wind"]["deg"]
 orientacion1=funcion(orientacion)
 
-print "La temperatura maxima de",prov,"es",temperaturamaxima,"grados centígrados."
-print "La temperatura minima de",prov,"es",temperaturaminima,"grados centígrados."
-print "La velocidad del viento en",prov,"es de",round(viento*1600/1000),"kilometros por hora en direccion",orientacion1,
+
+inserccion = plantilla.render(capital=prov,temp_min=temperaturaminima,temp_max=temperaturamaxima,viento1=viento,orientacion2=orientacion1)
+archivo = open('salida-html','w')
+archivo.write(inserccion)
+archivo.close()
+webbrowser.open("salida-html.html")
+
+
 
 
 
